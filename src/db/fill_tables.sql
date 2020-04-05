89e33bcfa44a9f8af617d2b769ba4c97728c6177
AndreiSerov <actsituation@gmail.com>
2020-04-05 18:21:24 +0700

    Add DB h2 Create tables in DB Fill users table

---
+++ src/db/fill_tables.sql
@@ -0,0 +1,25 @@
+insert into users (login, hash, salt) values (
+    'vasya',
+    'e5b0ab137b5a46379fd99d6068f87920b61488455df85d04c7737edf652264b40fe03049caf96ca7f63bb2f290f3145ce91e3cd699af0bfb6dac2b2b068c7950',
+    '89081'
+);
+
+insert into users (login, hash, salt) values (
+    "admin",
+    "26b6b57d98e7650c5071c3f4bf630e2051d6a25efac183a6bab213dc9bc31b600ab29695e76f9df7ca2a7ddf05ef9ef01078804f49a9c17b5c3441e62e569cb9",
+    "92867"
+);
+
+insert into users (login, hash, salt) values (
+    "q",
+    "97ed56b5fdd50b5b48132617783dc943d01bd1e9f31599d83ad85fd10df5cfb67819093bc334454c0cf0446087c331853e5d2511576509464c249a926bc52971",
+    "41290"
+);
+
+insert into users (login, hash, salt) values (
+    "abcdefghij",
+    "3de7d653fa01277e45de436d38902227f415918d36e124f649f7ef57820a7d8e5ad710ce58a8ab38e6d33e73a94f6562ee0c7e71d0fffe8eb3151137f631a94b",
+    "10216"
+);
+
+
---
+++ src/db/init_tables.sql
@@ -0,0 +1,28 @@
+CREATE TABLE users(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    login VARCHAR(10),
+    hash VARCHAR(64),
+    salt VARCHAR(5)
+);
+
+CREATE TABLE permissions(
+    id INT AUTO_INCREMENT,
+    user_id INT,
+    res VARCHAR(255),
+    role VARCHAR(7)
+    check (role in ('READ', 'WRITE', 'EXECUTE')),
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE sessions(
+    id INT AUTO_INCREMENT,
+    user_id VARCHAR(10) REFERENCES users(id),
+    permission_id INT,
+    ds VARCHAR(10),
+    de VARCHAR(10),
+    vol INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (permission_id) REFERENCES permissions(id)
+);
