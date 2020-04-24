241353108fa6beb50376ac9a70ac6ef528d90a59
AndreiSerov <actsituation@gmail.com>
2020-04-24 17:18:41 +0700

    Add html and jsp files. Finish Controller.kt logic

--- web/src/main/kotlin/com/dinosaur/app/servlet/Controller.kt
+++ web/src/main/kotlin/com/dinosaur/app/servlet/Controller.kt
@@ -5,22 +5,26 @@ import javax.servlet.http.HttpServlet
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
-@WebServlet(name = "Echo", value = ["/echo/*"])
+@WebServlet(name = "Echo", urlPatterns = ["echo/*", "echo/get", "echo/post"])
 class Controller: HttpServlet() {
 
-    override fun doGet(req: HttpServletRequest, res: HttpServletResponse) {
-        // TODO check cases "get123"
-        if (!req.requestURL.contains("/echo/get")) {
-            res.sendError(404, "Page not found")
-        } else {
-            val id = req.getParameter("id")
-            res.writer.println(id)
-        }
-    }
+    override fun doGet(req: HttpServletRequest,res: HttpServletResponse) =
+            // TODO check cases "get123"
+            if (!req.requestURL.contains("/echo/get")) {
+                res.sendError(404, "Page not found. GET")
+            } else {
+                val id = req.getParameter("id")
+                req.setAttribute("id", id)
+                req.getRequestDispatcher("../res.jsp")
+                        .forward(req, res)
+            }
 
     override fun doPost(req: HttpServletRequest, res: HttpServletResponse) {
         if (!req.requestURL.contains("/echo/post")) {
-            res.sendError(404, "Page not found")
+            res.sendError(404, "Page not found. POST")
+        } else {
+            val id = req.getParameter("input")
+            res.sendRedirect("get?id=$id")
         }
     }
 }
---
+++ web/src/main/webapp/res.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: andrei
+  Date: 24.04.2020
+  Time: 16:20
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+    <title>Your ID</title>
+</head>
+<body>
+    <h1>Your ID</h1>
+    <h1 style="color: green;">${id}</h1>
+</body>
+</html>
