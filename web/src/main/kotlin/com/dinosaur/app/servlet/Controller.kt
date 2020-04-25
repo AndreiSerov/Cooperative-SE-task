78949c9ef66d062688c714790d00482a2489cf30
AndreiSerov <actsituation@gmail.com>
2020-04-25 21:31:27 +0700

    fix russian

--- web/src/main/kotlin/com/dinosaur/app/servlet/Controller.kt
+++ web/src/main/kotlin/com/dinosaur/app/servlet/Controller.kt
@@ -1,5 +1,6 @@
 package com.dinosaur.app.servlet
 
+import java.net.URLEncoder.encode
 import javax.servlet.annotation.WebServlet
 import javax.servlet.http.HttpServlet
 import javax.servlet.http.HttpServletRequest
@@ -8,23 +9,25 @@ import javax.servlet.http.HttpServletResponse
 @WebServlet(name = "Echo", urlPatterns = ["echo/*", "echo/get", "echo/post"])
 class Controller: HttpServlet() {
 
-    override fun doGet(req: HttpServletRequest,res: HttpServletResponse) =
-            // TODO check cases "get123"
-            if (!req.requestURL.contains("/echo/get")) {
-                res.sendError(404, "Page not found. GET")
-            } else {
-                val id = req.getParameter("id")
-                req.setAttribute("id", id)
-                req.getRequestDispatcher("../res.jsp")
-                        .forward(req, res)
-            }
+    override fun doGet(req: HttpServletRequest,res: HttpServletResponse) {
+        // TODO check cases "get123"
+        if (!req.requestURL.contains("/echo/get")) {
+            res.sendError(404, "Page not found. GET")
+        } else {
+            val id = req.getParameter("id")
+            req.setAttribute("id", id)
+            println(req.queryString)
+            req.getRequestDispatcher("../res.jsp")
+                    .forward(req, res)
+        }
+    }
 
     override fun doPost(req: HttpServletRequest, res: HttpServletResponse) {
         if (!req.requestURL.contains("/echo/post")) {
             res.sendError(404, "Page not found. POST")
         } else {
             val id = req.getParameter("input")
-            res.sendRedirect("get?id=$id")
+            res.sendRedirect("get?id=${encode(id)}")
         }
     }
 }
